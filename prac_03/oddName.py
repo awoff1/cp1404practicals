5bba7a2ac9594c28303537dcddaca87a493f39b2
Alex <alex.woffenden@my.jcu.edu.au>
2018-04-05 13:44:05 +1000

    Refactor to use functions, add variable frequency

--- prac_03/oddName.py
+++ prac_03/oddName.py
@@ -1,8 +1,22 @@
 """Alex Woffenden"""
 
-name = input("Enter name:")
-while name == "" :
-    print("Name cannot be blank")
-    name = input("Enter name:")
 
-print(name[::2])
+def main():
+
+    name = get_name()
+    frequency = int(input("Enter frequency:"))
+    print_name(name,frequency)
+
+
+def get_name():
+    name = input("Enter name:")
+    while name == "":
+        print("Name cannot be blank")
+        name = input("Enter name:")
+    return name
+
+
+def print_name(name,frequency):
+    print(name[::frequency])
+
+main()
