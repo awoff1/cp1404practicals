2c38ef1c41b1200b075cf5574b37bfdfe1e41c52
Alex <alex.woffenden@my.jcu.edu.au>
2018-04-13 00:22:25 +1000

    Reformat dictionary and add printing of states

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -5,13 +5,17 @@ File needs reformatting
 """
 
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
-STATE_NAMES = {"QLD":"Queensland", "NSW": "New South Wales", "NT" :"Northern Territory", "WA" : "Western Australia", "ACT":"Australian Capital Territory", "VIC":"Victoria", "TAS":"Tasmania"}
+STATE_NAMES = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
+               "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 # print(STATE_NAMES)
 
-state = input("Enter short state: ")
+for state_name in STATE_NAMES:
+    print("{:3} is {}".format(state_name, STATE_NAMES[state_name]))
+
+state = input("Enter short state: ").upper()
 while state != "":
     if state in STATE_NAMES:
         print(state, "is", STATE_NAMES[state])
     else:
         print("Invalid short state")
-    state = input("Enter short state: ")
+    state = input("Enter short state: ").upper()
