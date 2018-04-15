adbb8797064f6407936fea5b98f1f44d2e137057
Alex <alex.woffenden@my.jcu.edu.au>
2018-04-16 00:41:23 +1000

    Initial commit

---
+++ prac_07/guitar_test.py
@@ -0,0 +1,10 @@
+from prac_07.guitar import Guitar
+
+gibson = Guitar("Gibson L-5 CES", 1922, 16035.40)
+another_guitar = Guitar("Another Guitar", 2012, 512.00)
+
+print("{} get_age() - Expected 96. Got {}".format(gibson.name, gibson.get_age()))
+print("{} get_age() - Expected 6. Got {}".format(another_guitar.name, another_guitar.get_age()))
+
+print("{} is_vintage() - Expected True. Got {}".format(gibson.name,gibson.is_vintage()))
+print("{} is_vintage() - Expected False. Got {}".format(another_guitar.name,another_guitar.is_vintage()))
